65cfdc9bbf05860d6a93fd2329d262dbf3a2129b
Arnab Chowdhury <chowdhuryarnab066@gmail.com>
2026-08-30 23:43:33 +0530

    Update auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,12 +1,10 @@
 import jwt from 'jsonwebtoken';
-
 const auth = (req, res, next) => {
   const authHeader = req.header('Authorization');
   
   if (!authHeader) {
     return res.status(401).json({ msg: 'No token, authorization denied' });
   }
-
   const parts = authHeader.split(' ');
   if (parts.length !== 2 || parts[0] !== 'Bearer') {
     return res.status(401).json({ msg: 'Token format is invalid, must be "Bearer <token>"' });
