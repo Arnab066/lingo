4c1fdddeceec254337986fa1c9d01ca6c43e39e9
Arnab Chowdhury <chowdhuryarnab066@gmail.com>
2026-09-18 22:24:52 +0530

    Clean up server.js by removing blank lines
    Removed unnecessary blank lines in server.js

--- server/server.js
+++ server/server.js
@@ -4,12 +4,9 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.js';
 import progressRoutes from './routes/progress.js';
-
 dotenv.config();
-
 const app = express();
 const PORT = process.env.PORT || 5000;
-
 // Middleware
 app.use(cors());
 app.use(express.json());
