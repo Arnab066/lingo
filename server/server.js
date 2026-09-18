dee8f82bc9878c448130564a959cd4a69512abce
Arnab Chowdhury <chowdhuryarnab066@gmail.com>
2026-09-18 22:27:50 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,6 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/progress', progressRoutes);
